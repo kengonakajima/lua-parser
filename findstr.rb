95979feb3a2860d70fa2694c73c68c4e5f11e2e4
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-06-18 06:48:20 +0900

    len

--- findstr.rb
+++ findstr.rb
@@ -15,7 +15,7 @@ def findstring(s)
     stype = :LONGSTR
     longtop = $&
     chars = s[longtop.size..-1].split("")
-    print "LONG! TOP:#{longtop} chars.size:#{chars.size} longtop.size:#{longtop.size}\n"
+#    print "LONG! TOP:#{longtop} chars.size:#{chars.size} longtop.size:#{longtop.size}\n"
   else
     return false
   end
@@ -55,35 +55,32 @@ def findstring(s)
     end
 
     if ch == "]" and longtop then 
-      print "LONG-CLOSING? "
       tofetch = longtop.size - 1 
       fetched = chars[0..(tofetch-1)]
       ok=true
       fetched.size.times do |i|
         if i==fetched.size-1 then
           if fetched[i]!="]" then
-            print "FALSE 2 "
             ok = false
             break
           end
         else
           if fetched[i] != "=" then 
-            print "FALSE 3 "
             ok = false
             break
           end
         end
       end
       if ok then 
-        return out
+        return out, longtop.size*2 + out.size
       end
     end
 
     if ch == "\"" and stype == :NORMALSTR then
-      return out
+      return out, out.size + 2
     end
     if ch == "'" and stype == :CHARSTR then
-      return out
+      return out, out.size + 2
     end
 
     if ch == "\\" then
@@ -98,47 +95,52 @@ end
 
 
 def expecteq(t,a,b)
-  if a==b then
-    print t,"\n"
+  res,uselen = findstring(a)
+  if res==b then
+    if b==false then
+      print t,"\n"
+    else
+      print t," #{res.size},#{uselen}\n"
+    end
   else
     raise "FATAL: #{t} : A:'#{a}' B:'#{b}'\n"
   end
 end
 
 #nonstring
-expecteq( "notstring", findstring("a=\"ahoあほ\"") , false )
+expecteq( "notstring", "a=\"ahoあほ\"", false )
 
 # normalstring
-expecteq( "nstrfull", findstring("\"ahoあほ\"") , "ahoあほ" )
-expecteq( "nstr", findstring("\"ahoあほ\"ahoaho") , "ahoあほ" )
-expecteq( "nstresc", findstring("\"ahoあほ\\\"aho\"ahoaho") , "ahoあほ\\\"aho" )
-expecteq( "nstresc2", findstring("\"ahoあほ\\\\aho\"ahoaho") , "ahoあほ\\\\aho" )
-expecteq( "nstrescetc", findstring("\"ahoあほ\\b\\t\\n\\f\\raho\"ahoaho") , "ahoあほ\\b\\t\\n\\f\\raho" )
-expecteq( "nstrnotterm", findstring("\"ahoあほ\\\"ahoahoaho") , false )
-expecteq( "nstrnl", findstring("\"ahoあほ\nahoaho") , false )
-expecteq( "nstru1", findstring("\"aho\\0aho\"hoge" ), "aho\\0aho" )
-expecteq( "nstru2", findstring("\"aho\\01aho\"hoge" ), "aho\\01aho" )
-expecteq( "nstru3", findstring("\"aho\\012aho\"hoge" ), "aho\\012aho" )
+expecteq( "nstrfull", "\"ahoあほ\"", "ahoあほ" )
+expecteq( "nstr", "\"ahoあほ\"ahoaho", "ahoあほ" )
+expecteq( "nstresc", "\"ahoあほ\\\"aho\"ahoaho", "ahoあほ\\\"aho" )
+expecteq( "nstresc2", "\"ahoあほ\\\\aho\"ahoaho", "ahoあほ\\\\aho" )
+expecteq( "nstrescetc", "\"ahoあほ\\b\\t\\n\\f\\raho\"ahoaho", "ahoあほ\\b\\t\\n\\f\\raho" )
+expecteq( "nstrnotterm", "\"ahoあほ\\\"ahoahoaho", false )
+expecteq( "nstrnl", "\"ahoあほ\nahoaho", false )
+expecteq( "nstru1", "\"aho\\0aho\"hoge", "aho\\0aho" )
+expecteq( "nstru2", "\"aho\\01aho\"hoge", "aho\\01aho" )
+expecteq( "nstru3", "\"aho\\012aho\"hoge", "aho\\012aho" )
 
 
 # charstring
-expecteq( "chstrfull", findstring("'ahoあほ'"), "ahoあほ" )
-expecteq( "chstr", findstring("'ahoあほ'ahoaho") , "ahoあほ" )
-expecteq( "chstresc", findstring("'ahoあほ\\'aho'ahoaho") , "ahoあほ\\'aho" )
-expecteq( "chstresc2", findstring("'ahoあほ\\\\aho'ahoaho") , "ahoあほ\\\\aho" )
-expecteq( "chstrescetc", findstring("'ahoあほ\\b\\t\\n\\f\\raho'ahoaho") , "ahoあほ\\b\\t\\n\\f\\raho" )
-expecteq( "chstrnotterm", findstring("'ahoあほ\\'ahoahoaho") , false )
-expecteq( "chstrnl", findstring("'ahoあほ\naho") , false )
-expecteq( "chstru1", findstring("'aho\\0aho'hoge" ), "aho\\0aho" )
-expecteq( "chstru2", findstring("'aho\\01aho'hoge" ), "aho\\01aho" )
-expecteq( "chstru3", findstring("'aho\\012aho'hoge" ), "aho\\012aho" )
+expecteq( "chstrfull", "'ahoあほ'", "ahoあほ" )
+expecteq( "chstr", "'ahoあほ'ahoaho", "ahoあほ" )
+expecteq( "chstresc", "'ahoあほ\\'aho'ahoaho", "ahoあほ\\'aho" )
+expecteq( "chstresc2", "'ahoあほ\\\\aho'ahoaho", "ahoあほ\\\\aho" )
+expecteq( "chstrescetc", "'ahoあほ\\b\\t\\n\\f\\raho'ahoaho", "ahoあほ\\b\\t\\n\\f\\raho" )
+expecteq( "chstrnotterm", "'ahoあほ\\'ahoahoaho", false )
+expecteq( "chstrnl", "'ahoあほ\naho", false )
+expecteq( "chstru1", "'aho\\0aho'hoge" , "aho\\0aho" )
+expecteq( "chstru2", "'aho\\01aho'hoge", "aho\\01aho" )
+expecteq( "chstru3", "'aho\\012aho'hoge", "aho\\012aho" )
 
 # longstring
-expecteq( "lstrfull", findstring("[[ahoあほ]]"), "ahoあほ" )
-expecteq( "lstrnl", findstring("[[aho\nあほ]]"), "aho\nあほ" )
-expecteq( "lstrnl2", findstring("[[\naho\nあほ\n]]"), "\naho\nあほ\n" )  # TODO: lua skips topline "\n"
-expecteq( "lstr3", findstring("[===[\naho\nあほ]===]"), "\naho\nあほ" )  # TODO: lua skips topline "\n"
-expecteq( "lstr3_1", findstring("[===[\naho\nあほ\n]===]"), "\naho\nあほ\n" )  # TODO: lua skips topline "\n"
-expecteq( "lstr3_1_esc", findstring("[===[\naho\n\"'あほ\\012aho\n]===]"), "\naho\n\"'あほ\\012aho\n" )  # TODO: lua skips topline "\n"
+expecteq( "lstrfull", "[[ahoあほ]]", "ahoあほ" )
+expecteq( "lstrnl", "[[aho\nあほ]]", "aho\nあほ" )
+expecteq( "lstrnl2", "[[\naho\nあほ\n]]", "\naho\nあほ\n" )  # TODO: lua skips topline "\n"
+expecteq( "lstr3", "[===[\naho\nあほ]===]", "\naho\nあほ" )  # TODO: lua skips topline "\n"
+expecteq( "lstr3_1", "[===[\naho\nあほ\n]===]", "\naho\nあほ\n" )  # TODO: lua skips topline "\n"
+expecteq( "lstr3_1_esc", "[===[\naho\n\"'あほ\\012aho\n]===]", "\naho\n\"'あほ\\012aho\n" )  # TODO: lua skips topline "\n"
 
 
